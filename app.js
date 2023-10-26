fd932c7d6818dcb614259fefda9c906b06a51939
alperenbekci <alperenbekci@hotmail.com>
2023-10-26 03:18:12 +0300

    v1 release 2 app

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const { generateMeta, generateImage } = require('./controllers/openaiController')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const {
+  generateMeta,
+  generateImage,
+} = require("./controllers/openaiController");
 
 // app setup
-const app = express()
-app.listen(4000, () => console.log('listening to requests on port 4000'))
+
+app.listen(4000, () => console.log("listening to requests on port 4000"));
 
 // middleware
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
 // routes
-app.post('/openai/meta', generateMeta)
-app.post('/openai/image', generateImage)
+app.post("/openai/meta", generateMeta);
+app.post("/openai/image", generateImage);
--- public/index.js
+++ public/index.js
@@ -1,39 +1,39 @@
 // forms
-const metaForm = document.querySelector('.meta-form')
-const imageForm = document.querySelector('.image-form')
+const metaForm = document.querySelector(".meta-form");
+const imageForm = document.querySelector(".image-form");
 
 // output elements
-const description = document.querySelector('.description p')
-const tags = document.querySelector('.tags p')
-const thumbnail = document.querySelector('.thumbnail img') 
+const description = document.querySelector(".description p");
+const tags = document.querySelector(".tags p");
+const thumbnail = document.querySelector(".thumbnail img");
 
 // description and tags
-metaForm.addEventListener('submit', async (e) => {
-  e.preventDefault()
+metaForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-  const res = await fetch('/openai/meta', {
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({title: metaForm.title.value}),
-    method: 'POST'
-  })
-  const data = await res.json()
+  const res = await fetch("/openai/meta", {
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: metaForm.title.value }),
+    method: "POST",
+  });
+  const data = await res.json();
 
-  console.log(data)
+  console.log(data);
 
-  description.textContent = data.description.content
-  tags.textContent = data.tags.content
-})
+  description.textContent = data.description.content;
+  tags.textContent = data.tags.content;
+});
 
 // image/thumbnail
-imageForm.addEventListener('submit', async (e) => {
-  e.preventDefault()
+imageForm.addEventListener("submit", async (e) => {
+  e.preventDefault(); // prevent page refresh
 
-  const res = await fetch('/openai/image', {
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({prompt: imageForm.prompt.value}),
-    method: 'POST'
-  })
-  const data = await res.json()
+  const res = await fetch("/openai/image", {
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ prompt: imageForm.prompt.value }),
+    method: "POST",
+  });
+  const data = await res.json();
 
-  thumbnail.setAttribute('src', data.url)
-})
+  thumbnail.setAttribute("src", data.url);
+});
