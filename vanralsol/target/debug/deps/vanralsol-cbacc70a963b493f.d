26781b4cf6dac339a020ccaabbb1751ace53639b
alperenbekci <alperenbekci@hotmail.com>
2023-10-26 12:33:17 +0300

    ranval backup

---
+++ vanralsol/target/debug/deps/vanralsol-cbacc70a963b493f.d
@@ -0,0 +1,5 @@
+/home/tugce/vanralsol/target/debug/deps/vanralsol-cbacc70a963b493f.rmeta: src/main.rs
+
+/home/tugce/vanralsol/target/debug/deps/vanralsol-cbacc70a963b493f.d: src/main.rs
+
+src/main.rs:
---
+++ vanralsol/target/debug/deps/vanralsol-d2a8a450c50cc746.d
@@ -0,0 +1,5 @@
+/home/tugce/vanralsol/target/debug/deps/vanralsol-d2a8a450c50cc746.rmeta: src/main.rs
+
+/home/tugce/vanralsol/target/debug/deps/vanralsol-d2a8a450c50cc746.d: src/main.rs
+
+src/main.rs:
